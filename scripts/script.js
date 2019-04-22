0d0141b07da2db76859a194bccf67a99858daf8f
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2019-04-21 23:51:06 -0500

    Newest Version

--- scripts/script.js
+++ scripts/script.js
@@ -47,20 +47,6 @@ $(document).scroll(function() {
 
 
 
-/*
-$('.mobile-section').click(function(){
-
-	$('.mobile-section').css('height', '100vh');
-	$('.mobile-section').css('position', 'fixed');
-	$('.mobile-section').css('top', '0');
-	$('.mobile-section').css('margin-top', '0px');
-
-
-});
-*/
-
-
-
 
 
 
@@ -68,14 +54,17 @@ $('.mobile-section').click(function(){
 var martop, marginDiff;
 $('.mobile-section').click(function(){
 
-	// Turns off the ability to scroll
+	// Turns off the ability to scroll and hides nav
 	toggleOverflow();
+	//hideShowNav();
+
+	$('.tri-con').css('display', 'block');
+
 
 	// Collects the margin top distance for object image placement
 	martop = $(this).css('margin-top');
 	martop = parseInt(martop);
 	marginDiff = martop-scrolled;
-
 
 	// Mobile image is made fixed and placed correctly relative to the division
 	$(".mob-image").css({
@@ -83,27 +72,63 @@ $('.mobile-section').click(function(){
 		"top": "0",
 		"top": marginDiff+'px'});
 	
-
 	// Makes division marks animate, separate 
 	$('.split').css('height', '100vh');
 	$('.split1').css('margin-top', '-100vh');
+	$('.split2').css('margin-top', '0vh');
 	$('.fill').css('height', '100%');
-
+	$('.fill1').css('bottom', '0');
+	$('.fill2').css('top', '0');
 
 	setTimeout(function(){
 		$('.mi').css('clip-path', 'none');
-	}, 100);
-
-	setTimeout(function(){
-		$('.mobile-section').css('position', 'fixed');
-		$('.mobile-section').css('height', '100vh');
-		$('.mobile-section').css('top', '0');
-
-	}, 300);
-
+	}, 175);
 
 
 });
+
+
+
+
+
+
+
+// Function to shrink and return division to its default status 
+$('#h-b').click(function(){
+
+	// Toggle the navigation elements and enable scroll
+	toggleOverflow();
+	//hideShowNav();
+
+	$('.tri-con').css('display', 'none');
+
+	setTimeout(function(){
+		$('.mi').css('clip-path', 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)');
+	}, 300);
+
+
+	$('.split').css('height', '175px');
+	$('.split1').css('margin-top', '-175px');
+	$('.split2').css('margin-top', '75px');
+	$('.fill').css('height', '0%');
+
+
+	$(".mob-image").css({
+		"position": "absolute", 
+		"top": "0",
+		"top": '0px'});
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -129,14 +154,20 @@ function toggleOverflow (){
 
 
 
+var navVis = true;
+function hideShowNav (){
+	if(navVis) {
+		$('.nav-wrap').css('display', 'none');
+		navVis = false;
+	} else {
+		$('.nav-wrap').css('display', 'block');
+		navVis = true;
+	}
+
+}
 
 
-$('#h-b').click(function(){
 
-	toggleOverflow();
-
-
-});
 
 
 
