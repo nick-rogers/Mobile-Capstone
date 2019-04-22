20dfac2acfa5dad552bf8b9c2e4234a89d80b9d7
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2019-04-21 20:42:29 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
 
 
 
-
+var open = false;
 
 $('.prog').click(function(){
 
-	$('.prog').css('width', '150px');
-
+	if(open){
+		$('.prog').css('width', '30px');
+		open = false;
+	}else {
+		$('.prog').css('width', '150px');
+		open = true;
+	}
 
 });
 
@@ -23,6 +28,53 @@ $(document).scroll(function() {
     console.log($(document).scrollTop());
     $('.p-bar').css('height', Math.round($(document).scrollTop()/27)+'px');
 })
+
+
+
+
+
+
+
+
+
+
+
+/*
+$('.mobile-section').click(function(){
+
+	$('.mobile-section').css('height', '100vh');
+	$('.mobile-section').css('position', 'fixed');
+	$('.mobile-section').css('top', '0');
+	$('.mobile-section').css('margin-top', '0px');
+
+
+});
+*/
+
+
+
+
+
+$('.mobile-section').click(function(){
+
+	$('.split').css('height', '100vh');
+	$('.split1').css('margin-top', '-100vh');
+	$('.fill').css('height', '100%');
+
+	$('.mi').css('clip-path', 'none');
+
+
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
