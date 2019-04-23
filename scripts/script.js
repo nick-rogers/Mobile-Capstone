525c6fd2ccf94bf6d47edf39ef856a46a8213c9a
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2019-04-22 20:57:51 -0500

    Full Objects

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,152 @@
 $(document).ready(function(){
 
+	// Object #1 
+	var ob01Title = "GNOMON";
+	var ob01Year = "2300 BCE";
+	var ob01Desc = "This Gnomon, or measuring stick, was found at the Chinese ruins of Taosi. The stick was used to measure the angle of sunlight and moonlight, and was among the earliest human attempts to measure our time.";
+
+	// Object #2 
+
+	var ob02Title = "STONEHENGE";
+	var ob02Year = "2000 BCE";
+	var ob02Desc = "Stonehenge, a stone monument in the British countryside, has perplexed archaeologist since its discovery. The monument was likely used by the english to to measure the seasons and for astronomy.";
+	// Object #3 
+
+	var ob03Title = "WATER CLOCK OF KARNAK";
+	var ob03Year = "1400 BCE";
+	var ob03Desc = "This water clock, created in modern-day Egypt, was used to mark out work times in Egypt. The bowl slowly emptied over a predictable duration, allowing the Egyptians to measure certain tasks. ";
+	// Object #4
+
+	var ob04Title = "LUXOR OBELISK";
+	var ob04Year = "1290 BCE";
+	var ob04Desc = "The Luxor Obelisk, created in Egypt and later taken to France, is an early example of monuments as timekeeping devices. The movement of the sun’s shadow and its angle allowed Egyptians to estimate the time of day.";
+
+	// Object #5
+
+	var ob05Title = "WORKMAN'S SUNDIAL";
+	var ob05Year = "1100 BCE";
+	var ob05Desc = "The workman's sundial was discovered in a working man's dwelling, dated to the 2nd millenia BCE, this sundial provides an early example of personal timekeeping for work purposes.";
+
+
+	// Object #6
+
+	var ob06Title = "GNOMON";
+	var ob06Year = "500 BCE";
+	var ob06Desc = "The ancient Persian empire used water clocks to measure time. This bowl with perforations in the bottom, allow the owner to pace out a uniform amount of time with only liquid and the bowl. ";
+	
+	// Object #7
+
+	var ob07Title = "HAN SUNDIAL";
+	var ob07Year = "150 BCE";
+	var ob07Desc = "The Han sundial, created during the Han dynasty period, has a square base and allowed the owner to measure the day very precisely with dozens of pegs.";
+
+	// Object #8
+
+	var ob08Title = "TOWER OF THE WINDS";
+	var ob08Year = "50 BCE";
+	var ob08Desc = "This building was created in Athens during the 1st century BCE, as the world’s first timekeeping tower. The tower also measured wind speeds and weather.";
+
+	// Object #9
+
+	var ob09Title = "SOLARIUM AUGUSTI";
+	var ob09Year = "10 BCE";
+	var ob09Desc = "Solarium August, was ordered by emperor augustus of Rome. It took the form of a cap on an existing Egyptian obelisk, the angle cast which measured the time of day. ";
+	// Object #10
+
+	var ob10Title = "ROMAN SUNDIAL";
+	var ob10Year = "50 CE";
+	var ob10Desc = "The Roman empire’s rise brought about a new era of growth in timekeeping. This marble timekeeping sundial housed in Rome brought new lavish styles to the practice of timekeeping. ";
+	// Object #11
+
+	var ob11Title = "STANDING STONE";
+	var ob11Year = "650 CE";
+	var ob11Desc = "The standing stone, located in Ireland’s countryside, is a stone-carved monument which casts a shadow to measure time. It is a part cultural and folk marker and a timekeeping device. ";
+	// Object #12
+
+	var ob12Title = "SU SONG'S TOWER";
+	var ob12Year = "1070 CE";
+	var ob12Desc = "Su Song, a chinese polymath who lived in the 11th century CE, created a sophisticated mechanical clock tower, which is not extant today. However, his plans and schematics have come down to us. ";
+	// Object #13
+
+	var ob13Title = "INCENSE CLOCK";
+	var ob13Year = "1110 CE";
+	var ob13Desc = "The incense clock was powered by heated powder that emanated out of a sculpture, in this case a dragon. The scents would change over the course of the burning, thus informing the owner what time of day it was. ";
+	// Object #14
+
+	var ob14Title = "ELEPHANT CLOCK";
+	var ob14Year = "1200 CE";
+	var ob14Desc = "The elephant clock was created by famed Arab medieval inventor and engineer Al-Jazari. The clock’s mechanism was based off of Indian and Chinese clocks. ";
+	// Object #15
+
+	var ob15Title = "SALISBURY TOWER";
+	var ob15Year = "1386 CE";
+	var ob15Desc = "The clockworks of the Salisbury tower are possibly the world’s first instance of a modern clock tower. The system worked a series of pulleys that rang a series of bells for prayer purposes. ";
+	// Object #16
+
+	var ob16Title = "POMANDER";
+	var ob16Year = "1530 CE";
+	var ob16Desc = "The pomander watch was a luxury item created by early watchmakers in modern-day Germany. The watch chimed on the hour, but did not have a visible clock-face.";
+	// Object #17
+
+	var ob17Title = "PENDULUM CLOCK";
+	var ob17Year = "1656 CE";
+	var ob17Desc = "The creation of the Pendulum clock by Christiaan Huygens marked a turning-point in timekeeping, as the pendulum model powered most clocks until the quartz watch’s creation. ";
+	// Object #18
+
+	var ob18Title = "RAILROAD CHRONOMETER";
+	var ob18Year = "1850 CE";
+	var ob18Desc = "With the arrival of the industrial revolution and the steam engine, trains revolutionized the way that time-measurement was carried out. More accurate times and thus more precise, synced clocks were produced.";
+	// Object #19
+
+	var ob19Title = "QUARTZ CLOCK";
+	var ob19Year = "1927";
+	var ob19Desc = "The Quartz clock revolution at the turn of the 20th century marked another new era in timekeeping. By generating an electric charge, the quartz powers the clock’s face and reduced the margin of error on time-keeping. ";
+	// Object #20
+
+	var ob20Title = "NIST QUARTZ STANDARD";
+	var ob20Year = "1929";
+	var ob20Desc = "The National Institute of Standards and Technology (NIST) used 4 quartz clocks to establish a new standard for American clocks. The organization allowed other clocks to be synced to their standards.";
+
+	// Object #21
+
+	var ob21Title = "NIST ATOMIC CLOCK";
+	var ob21Year = "1949";
+	var ob21Desc = "NIST created their own quartz clock after the establishment of their standards in 1927. This clock was the most accurate quartz clock in the world at the time, but was not available for the general public. ";
+	// Object #22
+
+	var ob22Title = "ASTROCHRON";
+	var ob22Year = "1967";
+	var ob22Desc = "The workman's sundial was discovered in a working man's dwelling, dated to the 2nd millenia BCE, this sundial provides an early example of personal timekeeping for work purposes.";
+
+	// Object #23
+
+	var ob23Title = "PULSAR LED WATCH";
+	var ob23Year = "1973";
+	var ob23Desc = "Sleek and modern in appearance, straight out of the space age, the so-called “Pulsar Time Computer”, was the world’s first digital watch, using an LED screen to display the time. ";
+	// Object #24
+
+	var ob24Title = "CASIO F-91W";
+	var ob24Year = "1991";
+	var ob24Desc = "The Casio digital watch, first manufactured by Japanese electronics company Casio in 1991, democratized digital watches. Durable and accurate, the watch is produced in the millions per year. ";
+	// Object #25
+
+	var ob25Title = "CHIP ATOMIC CLOCK";
+	var ob25Year = "2004";
+	var ob25Desc = "The world's smallest atomic clock, the micro chip atomic clock was created at NIST in 2003, and released in 2004. It allows the accuracy of atomic clocks to be brought to mobile devices. ";
+	// Object #26
+
+	var ob26Title = "STRONTIUM CLOCK";
+	var ob26Year = "2009";
+	var ob26Desc = "With this clock, researchers used multiple lasers to cool atoms and trap them in a grid of light. Then, a blue laser beam excites the cube-shaped cloud of the atoms. The vibrations of the atoms are measured and are incredibly regular. ";
+	// Object #27
+
+	var ob27Title = "YTTERBIUM CLOCK";
+	var ob27Year = "2018";
+	var ob27Desc = "The Ytterbium atomic clock is so accurate that it would be off by less than a single second in 14 billion years. It runs off of the half life of the element Ytterbium.";
 
 
 
 
-function updateSlider(slideAmount){
-	var sliderDiv = document.getElementById("vert-range");
-    console.log(sliderDiv);
-}
-
-
-
-
-
-
-
-//document.getElementById("vert-range").value = "75";
-
-
-
-
-
-
-// Scroll function 
-var scrolled = 0, percentage;
-$(document).scroll(function() {
-    scrolled = $(document).scrollTop();
-    $('.p-bar').css('height', Math.round($(document).scrollTop()/27)+'px');
-})
 
 
 
@@ -43,8 +162,59 @@ $(document).scroll(function() {
 
 
 
-var martop, marginDiff;
+
+
+
+
+
+
+
+
+
+
+
+
+	// STORE IN ARRAYS 
+
+	var objectInfo = [[ob01Title,ob01Year,ob01Desc],[ob02Title,ob02Year,ob02Desc],[ob03Title,ob03Year,ob03Desc],[ob04Title,ob04Year,ob04Desc],[ob05Title,ob05Year,ob05Desc],[ob06Title,ob06Year,ob06Desc],[ob07Title,ob07Year,ob07Desc],[ob08Title,ob08Year,ob08Desc],[ob09Title,ob09Year,ob09Desc],[ob10Title,ob10Year,ob10Desc],[ob11Title,ob11Year,ob11Desc],[ob12Title,ob12Year,ob12Desc],[ob13Title,ob13Year,ob13Desc],[ob14Title,ob14Year,ob14Desc],[ob15Title,ob15Year,ob15Desc],[ob16Title,ob16Year,ob16Desc],[ob17Title,ob17Year,ob17Desc],[ob18Title,ob18Year,ob18Desc],[ob19Title,ob19Year,ob19Desc],[ob20Title,ob20Year,ob20Desc],[ob21Title,ob21Year,ob21Desc],[ob22Title,ob22Year,ob22Desc],[ob23Title,ob23Year,ob23Desc],[ob24Title,ob24Year,ob24Desc],[ob25Title,ob25Year,ob25Desc],[ob26Title,ob26Year,ob26Desc],[ob27Title,ob27Year,ob27Desc]];
+
+
+	var cutcanvas;
+	var objectMove;
+	var textSelect;
+	var thumb;
+	var slice;
+
+	// Function to select object canvases 
+	var theX;
+	var theY;
+	var coor;
+
+	var cutcanvas;
+	var setID;
+	var img;
+	var percentage;
+	var mask;
+	var theY;
+
+
+
+
+
+
+
+
+var martop, marginDiff, obID, parsed;
 $('.mobile-section').click(function(){
+
+
+	obID = $(this).attr('id');
+	obID = obID.substring(6,8);
+	parsed = parseInt(obID);
+
+	fillContent(obID, parsed);
+
+
 
 	// Turns off the ability to scroll and hides nav
 	toggleOverflow();
@@ -95,12 +265,36 @@ $('.mobile-section').click(function(){
 	}, 175);
 
 
-
-
-
 });
 
 
+
+
+
+
+
+
+
+//var cont; 
+function fillContent(id, parsed){
+
+	console.log('obID: ' + id + " parsed: " + parsed);
+
+
+	if(id.substring(0,1) < 1){
+		//console.log('less');
+		$('#mob-object-title').text(objectInfo[((id.substring(1,2))-1)][0]);
+		$('#mob-date').text(objectInfo[((id.substring(1,2))-1)][1]);
+		$('#mob-body').text(objectInfo[((id.substring(1,2))-1)][2]);
+	}else {
+		//console.log('more');
+		$('#mob-object-title').text(objectInfo[((id.substring(0,2))-1)][0]);
+		$('#mob-date').text(objectInfo[((id.substring(0,2))-1)][1]);
+		$('#mob-body').text(objectInfo[((id.substring(0,2))-1)][2]);
+	}
+
+
+}
 
 
 
@@ -143,6 +337,64 @@ $('#mob-x').click(function(){
 		"top": '0px',
 		/*"transform": "scale(1)"*/});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function updateSlider(slideAmount){
+	var sliderDiv = document.getElementById("vert-range");
+    console.log(sliderDiv);
+}
+
+
+
+
+
+
+
+//document.getElementById("vert-range").value = "75";
+
+
+
+
+
+
+// Scroll function 
+var scrolled = 0, percentage;
+$(document).scroll(function() {
+    scrolled = $(document).scrollTop();
+    $('.p-bar').css('height', Math.round($(document).scrollTop()/27)+'px');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
